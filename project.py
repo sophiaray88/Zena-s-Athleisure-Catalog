5e0415d601332f7db56828a77f5a4dfc607a3ec0
sophiaray88 <sophiafelicity@gmail.com>
2024-08-25 10:40:36 -0500

    Create project.py
    Documenting images display

---
+++ project.py
@@ -0,0 +1,8 @@
+The app has two file: streamlit_app.py and environment.yml in snowflake.
+
+
+There are two options for getting the images to display. You can either:
+
+1. Put copies of the images in the same stage as the app, or
+2. Use pre-signed URLs and give your app access to the stage where you originally stored your sweatsuit images.
+Pre-signed URLs are a way for you to serve images to external users by giving them a link that has permissions for a limited period of time. 
