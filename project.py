a3e2c0ed0feb06ae93e3aa8ef5ea55d83a69885c
sophiaray88 <sophiafelicity@gmail.com>
2024-08-25 22:37:15 -0500

    Update project.py

--- project.py
+++ project.py
@@ -6,3 +6,8 @@ There are two options for getting the images to display. You can either:
 1. Put copies of the images in the same stage as the app, or
 2. Use pre-signed URLs and give your app access to the stage where you originally stored your sweatsuit images.
 Pre-signed URLs are a way for you to serve images to external users by giving them a link that has permissions for a limited period of time. 
+
+  
+types of data: GeoSpatial Data 
+  --Navigate to docs.snowflake.com and search for "GeoSpatial Functions." You should see a list of functions. 
+https://docs.snowflake.com/en/sql-reference/functions-geospatial 
